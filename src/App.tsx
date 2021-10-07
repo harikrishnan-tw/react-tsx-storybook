051b108ae0d8db0ffc6d6b9923e367eacb370253
harikrishnan-tw <86928653+harikrishnan-tw@users.noreply.github.com>
2021-10-07 11:02:12 +0530

    added mui for react

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { Button } from "@mui/material";
 
 function App() {
   return (
     <div className='App'>
+      <Button variant='contained'>Hello World</Button>
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
         <p>
